fbbafe128eb05e2f7f31bef4d8fd150f439c4c9d
martas <m.susek@live.com>
2019-07-02 22:32:00 +0200

    Add pages components to app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import "./styles/index.scss"
 import Header from "./components/header"
+import { NavigationRoutes } from './routes';
+import Navigation from './components/navigation';
+
 const App: React.FC = () => {
   return (
     <div className="app">
       <Header></Header>
+      <Navigation></Navigation>
+      <NavigationRoutes></NavigationRoutes>
     </div>
   );
 }
