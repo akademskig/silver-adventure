7cac5d01df7c086b3e90dccecc7fb854126df496
martas <m.susek@live.com>
2019-07-03 10:10:56 +0200

    Route from base route

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,17 @@
 
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import allContactsPage from './pages/allContactsPage';
-import myFavouritesPage from './pages/myFavouritesPage';
+import myFavoritesPage from './pages/myFavoritesPage';
 
-export const NavigationRoutes = () => (
-    <Switch>
-        <Route path='/all_contacts' component={allContactsPage} />
-        <Route path='/my_favourites' component={myFavouritesPage} />
-    </Switch>
-)
+export default class NavigationRoutes extends React.Component {
+    render() {
+        return (
+            <Switch>
+                <Route exact path="/" render={() => <Redirect to="/all_contacts"></Redirect>}></Route>
+                <Route path='/all_contacts' component={allContactsPage} />
+                <Route path='/my_favorites' component={myFavoritesPage} />
+            </Switch>
+        )
+    }
+}
