56ddb5fd8bd0081442d8d85d05580016d75bcb61
martas <m.susek@live.com>
2019-07-04 14:06:41 +0200

    Add UserCards list.

--- src/pages/allContactsPage.tsx
+++ src/pages/allContactsPage.tsx
@@ -1,10 +1,11 @@
-import React from "react"
+import React, { Fragment } from "react"
+import UserCardList from "../containers/UserCardList";
 
 export default class allContactsPage extends React.Component {
 
     render() {
         return (
-            <div>allContacts</div>
+                <UserCardList></UserCardList>
         )
     }
 }
