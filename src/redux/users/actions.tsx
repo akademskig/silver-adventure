8abac2a71e362776b651e1058804fe8393010271
martas <m.susek@live.com>
2019-07-04 14:07:08 +0200

    Edit recieved data.

--- src/redux/users/actions.tsx
+++ src/redux/users/actions.tsx
@@ -1,18 +1,20 @@
 import { Dispatch } from "react";
 import { FetchUsersAction } from "./types";
-
 export const FETCH_USERS_BEGIN = "FETCH_USERS_BEGIN"
 export const FETCH_USERS_SUCCESS = "FETCH_USERS_SUCSESS"
 export const FETCH_USERS_ERROR = "FETCH_USERS_ERROR"
-
+const USERS_API = "https://randomuser.me/api/?results=22"
 export const fetchUsers = () => {
     return (dispatch: Dispatch<FetchUsersAction>) => {
         dispatch(fetchUsersBegin())
-        return fetch("/data/users.json")
+        return fetch(USERS_API)
             .then(handleErrors)
             .then(res => res.json())
             .then(data => {
-                dispatch(fetchUsersSuccess(data))
+                let editedData = data.results.map((r: any) => Object.assign({}, r, {
+                    phone_numbers: [{ home: r.phone }, { cell: r.cell }]
+                }))
+                dispatch(fetchUsersSuccess(editedData))
                 return data
             })
             .catch(error => dispatch(fetchUsersError(error)))
