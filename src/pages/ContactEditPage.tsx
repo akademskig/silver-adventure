cfdd2cdf979b77d33c74e67e43a82c0702cc19b9
martas <m.susek@live.com>
2019-07-07 09:46:38 +0200

    Ad ContactEdit and ContactView pages.

--- src/pages/ContactEditPage.tsx
+++ src/pages/ContactEditPage.tsx
@@ -1,9 +1,26 @@
 import React from "react"
-export default class ContactEditPage extends React.Component{
+import { connect } from "react-redux"
+import { withRouter } from "react-router";
+
+class ContactEditPage extends React.Component<any>{
 
     render(){
         return(
-            <div>Hello</div>
+            <div className="">Edit</div>
         )
     }
 }
+
+
+const mapStateToProps = (state: any) => {
+    return {
+        users: state.usersReducer.users,
+        loading: state.usersReducer.loading,
+        error: state.usersReducer.error,
+        user: state.usersReducer.user
+    }
+}
+
+
+
+export default connect(mapStateToProps)(withRouter(ContactEditPage))
