d6ee2780d7a4d7875248aa48dc7811bec72a0a59
martas <m.susek@live.com>
2019-07-07 09:52:12 +0200

    Update allContactsPage and myFavouritesPage.

--- src/pages/allContactsPage.tsx
+++ src/pages/allContactsPage.tsx
@@ -1,11 +1,15 @@
 import React from "react"
 import UsersList from "../containers/UsersList";
+import SearchBar from "../components/SearchBar";
+import Navigation from '../components/navigation';
 
 export default class allContactsPage extends React.Component {
 
     render() {
         return (
             <React.Fragment>
+                <Navigation></Navigation>
+                <SearchBar></SearchBar>
                 <UsersList></UsersList>
             </React.Fragment>
         )
--- src/pages/myFavoritesPage.tsx
+++ src/pages/myFavoritesPage.tsx
@@ -1,10 +1,13 @@
 import React from "react"
 import UsersList from "../containers/UsersList";
-
+import SearchBar from "../components/SearchBar";
+import Navigation from '../components/navigation';
 export default class myFavoritesPage extends React.Component {
     render() {
         return (
             <React.Fragment>
+                <Navigation></Navigation>
+                <SearchBar></SearchBar>
                 <UsersList></UsersList>
             </React.Fragment>
         )
