508300ed20b647c8b8961907d47f682ff5b38ec7
martas <m.susek@live.com>
2019-07-07 09:37:37 +0200

    Add link to "/" to header logo.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,13 @@
 import appLogo from "../assets/logos/logo.svg"
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const Header: React.FC = () => {
     return (
         <header className="app__header">
-            <img alt="app-logo" src={appLogo}></img>
+            <Link to="/">
+                <img alt="app-logo" src={appLogo}></img>
+            </Link>
             <div className="app__header__line">
             </div>
         </header>
