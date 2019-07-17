7c6df91401e1ae1b2cc08a2d99e4908c18abc8f5
martas <m.susek@live.com>
2019-07-17 15:59:13 +0200

    Save updated users list

--- src/components/contactView/ContactEdit.tsx
+++ src/components/contactView/ContactEdit.tsx
@@ -5,10 +5,16 @@ import NameBar from "./NameBar";
 import ProfilePhoto from "./ProfilePhoto";
 import ContactForm from "./ContactForm";
 import { addInitialValues } from "../../redux/actions/contactForm";
+import { User } from "../../types/user";
+import { saveUsers } from "../../assets/services/usersService";
 
 const ContactEdit = (props: any) => {
-    const { user, history, dispatch } = props
+    const { user, users, history, dispatch } = props
     dispatch(addInitialValues(user))
+    const onSubmit = (values: any) => {
+        const newUsers = users.map((u: User) => values.id === u.id ? values : u)
+        dispatch(saveUsers(newUsers))
+    }
     return (
         <React.Fragment>
             <IconsBarMobile {...history}></IconsBarMobile>
@@ -16,7 +22,7 @@ const ContactEdit = (props: any) => {
                 <ProfilePhoto {...props} imgType="edit"></ProfilePhoto>
                 <div className="contact-details__info">
                     <NameBar mode="edit"{...props}></NameBar>
-                    <ContactForm {...props} ></ContactForm>
+                    <ContactForm onSubmit={onSubmit}{...props} ></ContactForm>
                 </div>
             </div>
         </React.Fragment>
