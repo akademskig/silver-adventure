6f1df0a8369546f811ea45a2af2c0617806e0376
martas <m.susek@live.com>
2019-07-18 10:29:44 +0200

    Add edit link to user details icon.

--- src/components/contactView/NameBar.tsx
+++ src/components/contactView/NameBar.tsx
@@ -4,6 +4,7 @@ import iconBack from "../../assets/icons/arrow-back.svg"
 import iconHearthEmpty from "../../assets/icons/icon-hearth-empty.svg"
 import iconEdit from "../../assets/icons/icon-edit.svg"
 import iconTrash from "../../assets/icons/delete.svg"
+import { Link } from "react-router-dom";
 
 const NameBar = (props: any) => {
     const { user, history, mode } = props
@@ -13,20 +14,22 @@ const NameBar = (props: any) => {
     return (
         <div className="contact-name">
             <span className="contact-name__left">
-                <img className="icon icon-desktop-large"src={iconBack} onClick={onClickBack}></img>
+                <img className="icon icon-desktop-large" src={iconBack} onClick={onClickBack}></img>
                 {mode === "details" && <h2>{capitalize(user.full_name)} </h2>}
             </span>
             <span className="contact-name__right">
                 {mode === "details" &&
                     <React.Fragment>
-                        <img className="icon icon-desktop"src={iconHearthEmpty}></img>
-                        <img className="icon icon-desktop"src={iconEdit}></img>
+                        <img className="icon icon-desktop" src={iconHearthEmpty}></img>
+                        <Link to={`/contacts/${user.id}/edit`}>
+                            <img className="icon icon-desktop" src={iconEdit}></img>
+                        </Link>
                     </React.Fragment>
                 }
                 {mode === "edit" &&
                     <React.Fragment>
                         <span className="icon-text">Delete</span>
-                        <img className="icon icon-desktop"src={iconTrash}></img>
+                        <img className="icon icon-desktop" src={iconTrash}></img>
                     </React.Fragment>
                 }
 
