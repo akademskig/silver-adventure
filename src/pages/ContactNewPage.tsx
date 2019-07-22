4a0e7fa31715e97e4d1ab1a6a1c860970d9930fa
marta <m.susek@live.com>
2019-07-22 20:56:54 +0200

    Implement add new user.

--- src/pages/ContactNewPage.tsx
+++ src/pages/ContactNewPage.tsx
@@ -1,17 +1,17 @@
-import { History } from "history";
 import React from "react";
-
 import { withRouter } from "react-router";
-import { connectAdvanced, connect } from "react-redux";
 import IconsBarMobile from "../components/contactView/IconsBarMobile";
 import ProfilePhoto from "../components/contactView/ProfilePhoto";
 import NameBar from "../components/contactView/NameBar";
 import ContactForm from "../components/contactView/ContactForm";
+import { addNewUser } from "../services/usersService";
+import { addInitialValues } from "../redux/actions/contactForm";
 
 const ContactNewPage = (props: any) => {
     const { history, dispatch } = props
+    dispatch(addInitialValues(null))
     const onSubmit = (values: any) => {
-        // dispatch(addNewUser(values))
+        dispatch(addNewUser(values))
         history.goBack()
     }
     const onCancel = () => {
@@ -28,18 +28,12 @@ const ContactNewPage = (props: any) => {
                 </div>
             </div>
         </React.Fragment>
-
     )
-
 }
 
-const mapStateToProps = (state: any) => {
-    return {
-        userToEdit:{}
-    }
-}
 
-export default connect(mapStateToProps)(withRouter(ContactNewPage))
+
+export default withRouter(ContactNewPage)
 
 
 
--- src/redux/actions/contactForm.tsx
+++ src/redux/actions/contactForm.tsx
@@ -3,7 +3,7 @@ import { User } from "../../types/user";
 export const ADD_PHONE_NUMBER = "ADD_PHONE_NUMBER"
 export const ADD_INITIAL_VALUES = "ADD_INITIAL_VALUES"
 
-export const addInitialValues = (user: User) => {
+export const addInitialValues = (user: User | null) => {
     return ({
         type: ADD_INITIAL_VALUES,
         payload: { userToEdit: user }
