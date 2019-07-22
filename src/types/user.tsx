4aa43723587d0b79ea69dae6dad8a10c24131960
marta <m.susek@live.com>
2019-07-22 20:58:06 +0200

    Change USer interface to class.

--- src/types/user.tsx
+++ src/types/user.tsx
@@ -1,14 +1,28 @@
-export type User = {
-    id: number,
-    first_name: string,
-    last_name: string,
-    email: string,
-    phones: Array<PhoneItem>,
-    profile_photo: string
-}
+export class User {
+    id: string | null = null
+    full_name = ""
+    email = ""
+    phones = [{
+        label: "",
+        name: "",
+        number: ""
+    }]
+    profile_photo = ""
 
-interface PhoneItem {
-    label: string,
-    name: string,
-    number: string
+    constructor(user?: any) {
+        if (!user)
+            return
+        if (user.id)
+            this.id = user.id
+        else
+            this.id = Math.random().toString(36).substr(2, 16);
+        if (user.full_name)
+            this.full_name = user.full_name
+        if (user.email)
+            this.email = user.email
+        if (user.phones)
+            this.phones = user.phones
+        if (user.profile_photo)
+            this.profile_photo = user.profile_photo
+    }
 }
