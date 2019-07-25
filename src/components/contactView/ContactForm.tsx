19581c160ea8ea6d812677590f1371486a4eccc7
marta <m.susek@live.com>
2019-07-25 11:51:37 +0200

    Feature: Enable field validation.

--- src/components/contactView/ContactForm.tsx
+++ src/components/contactView/ContactForm.tsx
@@ -7,24 +7,17 @@ import validate from './contactFormValidate';
 import iconPerson from "../../assets/icons/person.svg"
 import iconPhone from "../../assets/icons/phone.svg"
 import iconEmail from "../../assets/icons/email.svg"
-let ContactForm = (props: any) => {
-    const { handleSubmit, submitting, reset, invalid, onCancel } = props
+import iconInfo from "../../assets/icons/info.svg"
 
+let ContactForm = (props: any) => {
+    const { handleSubmit, submitting, invalid, reset, onCancel } = props
     return (
-        <form className="contact-form" onSubmit={handleSubmit}>
+        <form className="contact-form" onSubmit={handleSubmit} >
             <div className="contact-form__form-section">
-                <label className="field-label" htmlFor="full_name">
-                    <img src={iconPerson} alt="Person icon"></img>
-                    full name
-                    </label>
-                <Field className="form-input name" name="full_name" component="input" type="text" />
+                <Field className="form-input name" name="full_name" component={renderField} type="text" />
             </div>
             <div className="contact-form__form-section">
-                <label className="field-label" htmlFor="email">
-                    <img src={iconEmail} alt="Email icon"></img>
-                    email
-                    </label>
-                <Field className="form-input name" name="email" component="input" type="email" />
+                <Field className="form-input email" name="email" component={renderField} type="email" />
             </div>
             <div className="contact-form__form-section">
                 <label className="field-label" htmlFor="phones">
@@ -35,30 +28,54 @@ let ContactForm = (props: any) => {
             </div>
 
             <div className="form-btns">
-                <button className="btn btn-cancel" type="reset" onClick={onCancel}>Cancel</button>
+                <button className="btn btn-cancel" type="reset" onClick={() => { reset(); onCancel() }}>Cancel</button>
                 <button className="btn btn-save" disabled={invalid || submitting} type="submit">Save</button>
             </div>
         </form>)
 }
 
-const renderNumberFields = (props: any) => {
-    const { fields, meta: { errors } } = props
+
+const renderField = (props: any) => {
+    const { input, label, type, htmlFor, className, meta: { touched, error } } = props
+    const inputClassName = touched && error ? `${className} error` : className
     return (
-        <div >
+        <span className={className}>
+            {input.name === "email" &&
+                <label className="field-label" htmlFor={htmlFor}>
+                    <img src={iconEmail} alt="Email icon"></img>
+                    email
+                    </label>
+            }
+            {input.name === "full_name" &&
+                <label className="field-label" htmlFor={htmlFor}>
+                    <img src={iconPerson} alt="User icon"></img>
+                    full name
+                    </label>
+            }
+            <input {...input} placeholder={label} type={type} className={inputClassName} name={input.name} />
+            {touched && ((error && <div className="error-message"><img src={iconInfo}></img><p>{error}</p></div>))}
+        </span>
+    )
+
+}
+const renderNumberFields = (props: any) => {
+    const { fields, meta: { error } } = props
+    return (
+        <React.Fragment>
             <ul className="numbers-list">
                 {fields && fields.map((phone: any, i: number) => {
                     return (
 
                         <li key={i} className="numbers-list__number">
-                            <Field className="form-input number__number" component="input" type="text" name={`${phone}.number`} />
-                            <Field className="number__name" name={`${phone}.name`} component="input" type="text" />
+                            <Field className="form-input number__number" component={renderField} type="text" name={`${phone}.number`} />
+                            <Field className="form-input number__name" name={`${phone}.name`} component={renderField} type="text" />
                             <button type="button" onClick={() => fields.remove(i)} value={i}>
                                 <img src={iconRemove} alt="Remove icon"></img>
                             </button>
                         </li>
                     )
                 })}
-                {errors && <li className="error">{errors}</li>}
+                {error && <li className="error">{error}</li>}
             </ul>
             <div className="add-numbers btn">
                 <button type="button" onClick={() => fields.push()} className="btn btn-add-number round" name="add-number">
@@ -66,14 +83,14 @@ const renderNumberFields = (props: any) => {
                 </button>
                 <label htmlFor="add-number">Add numbers</label>
             </div>
-        </div>
+        </React.Fragment>
 
     )
 }
 
 const ContactReduxForm = reduxForm({
     form: 'contact',
-    validate
+    validate: validate
 })(ContactForm)
 
 export default connect(
--- src/components/contactView/contactFormValidate.tsx
+++ src/components/contactView/contactFormValidate.tsx
@@ -1,36 +1,42 @@
-const validate =( values:any) => {
-    const errors:any= {}
-    if (!values.full_name) {
-      errors.first_name = 'Required'
-    }
-    if(!values.email){
-        errors.email = 'Required'
-    }
-    if (!values.phones || !values.phones.length) {
-      errors.phones = { _error: 'At least one phone number must be entered' }
-    } else {
-      const phonesErrors:any = []
-      values.phones.forEach((phone:any, phoneIndex:number) => {
-        const phoneErrors : any= {}
-        if (!phone || !phone.label) {
-          phoneErrors.label = 'Required'
-          phonesErrors[phoneIndex] = phoneErrors
-        }
-        if (!phone || !phone.name) {
-          phoneErrors.name = 'Required'
-          phonesErrors[phoneIndex] = phoneErrors
-        }
-        if (!phone || !phone.number) {
-            phoneErrors.number = 'Required'
-            phonesErrors[phoneIndex] = phoneErrors
-          }
-      })
-      if (phonesErrors.length) {
-        errors.phones = phonesErrors
-      }
-    }
-    console.log(errors)
-    return errors
+const validate = (values: any) => {
+  const errors: any = {}
+  if (!values.full_name) {
+    errors.full_name = 'This field is required'
   }
-  
-  export default validate
+  if (!values.email) {
+    errors.email = 'This field is required'
+  }
+  else if (!values.email.match("@"))
+    errors.email = 'Invalid email'
+
+  if (!values.phones || !values.phones.length) {
+    errors.phones = { _error: 'At least one phone number must be entered' }
+  } else {
+    const phonesErrors: any = []
+    values.phones.forEach((phone: any, phoneIndex: number) => {
+      const phoneErrors: any = {}
+      if (!phone || !phone.label) {
+        phoneErrors.label = 'This field is required'
+        phonesErrors[phoneIndex] = phoneErrors
+      }
+      if (!phone || !phone.name) {
+        phoneErrors.name = 'This field is required'
+        phonesErrors[phoneIndex] = phoneErrors
+      }
+      if (!phone || !phone.number) {
+        phoneErrors.number = 'This field is required'
+        phonesErrors[phoneIndex] = phoneErrors
+      }
+      else if (phone.number.match(/[a-zA-Z!@#$%^&*(),.?":{}|<>]/g)) {
+        phoneErrors.number = 'Invalid number'
+        phonesErrors[phoneIndex] = phoneErrors
+      }
+    })
+    if (phonesErrors.length) {
+      errors.phones = phonesErrors
+    }
+  }
+  return errors
+}
+
+export default validate
