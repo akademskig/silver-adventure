f00d0f64f91d8d08e9229102bdaf66a38b7ed25b
marta <m.susek@live.com>
2019-07-29 22:05:26 +0200

    Mod - EMptyUserCard : Use mode enum.

--- src/components/EmptyUserCard.tsx
+++ src/components/EmptyUserCard.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { UserListState } from "../redux/types";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import addIcon from "../assets/icons/add-opaque.svg"
+import { EmptyUserCardMode } from "../containers/types";
 class EmptyUserCard extends React.Component<any, UserListState, any>{
 
     render() {
-        const cardClass = this.props.mode === "mobile" ? "empty-card-mobile card mobile-card" : "empty-card card"
+        const { mode } = this.props
+        const cardClass = mode === EmptyUserCardMode.MOBILE ? "empty-card-mobile card mobile-card" : "empty-card card"
         return (
-            <Link to={`/contacts/new`}>
+            <Link to={`/contacts/new`} >
                 <div className={cardClass}>
                     <div className="empty-card__content">
                         <img src={addIcon} alt="Add icon"></img>
-                      <p>Add new</p>
+                        <p>Add new</p>
                     </div>
                 </div>
             </Link>
@@ -19,4 +21,4 @@ class EmptyUserCard extends React.Component<any, UserListState, any>{
     }
 }
 
-export default EmptyUserCard
+export default withRouter(EmptyUserCard)
---
+++ src/containers/types.tsx
@@ -0,0 +1,9 @@
+export enum EmptyUserCardMode {
+    DESKTOP = "desktop",
+    MOBILE = "mobile"
+}
+
+export enum Routes {
+    CONTACTS_ALL = "/contacts/all_contacts",
+    CONTACTS_FAVORITES = "/contacts/my_favorites"
+}
