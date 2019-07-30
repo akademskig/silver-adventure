7a303cf5a65d8bcf5752fe12017b6c973476c3f6
marta <m.susek@live.com>
2019-07-30 11:15:34 +0200

    Feature: Add initSearch function.

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,21 +1,37 @@
-import React from "react"
+import React, { RefObject } from "react"
 import searchIcon from "../assets/icons/search.svg"
 import { connect } from "react-redux"
 import { UserListState } from "../redux/types";
 import { filterUsers } from "../utils";
 import { searchUsers } from "../redux/actions/users";
 class SearchBar extends React.Component<any, UserListState, any>{
+    searchValue = ""
+    inputRef: RefObject<any>
+    constructor(props: any) {
+        super(props)
+        this.inputRef = React.createRef()
+    }
     handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const users = filterUsers(this.props.users, e.target.value)
+        this.searchValue = e.target.value
+        const users = filterUsers(this.props.users, this.searchValue)
         this.props.dispatch(searchUsers(users))
     }
 
+    initSearch = () => {
+        if (this.inputRef.current) {
+            const users = filterUsers(this.props.users, this.inputRef.current.value)
+            this.props.dispatch(searchUsers(users))
+        }
+    }
+
     render() {
+        this.initSearch()
         return (
             <div className="search-container">
                 <div className="search">
                     <img alt="Search icon" className="search__icon" src={searchIcon}></img>
                     <input
+                        ref={this.inputRef}
                         onChange={this.handleChange}
                         className="search__input"
                         type="text"
